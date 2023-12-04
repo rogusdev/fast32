ce03b94fb063cdbe35c2d0690993e7f51a56a2ea
Chris Rogus <rogusdev@gmail.com>
2023-12-03 18:39:49 -0700

    encode and decode u64

---
+++ src/encode.rs
@@ -0,0 +1,178 @@
+pub const ENC_CROCKFORD_UPPER: &'static [u8; 32] = b"0123456789ABCDEFGHJKMNPQRSTVWXYZ";
+
+const B32_U64_C12_MSK: u64 = 0b1111000000000000000000000000000000000000000000000000000000000000;
+const B32_U64_C12_MIN: u64 = 0b0001000000000000000000000000000000000000000000000000000000000000;
+const B32_U64_C11_MSK: u64 = 0b0000111110000000000000000000000000000000000000000000000000000000;
+const B32_U64_C11_MIN: u64 = 0b0000000010000000000000000000000000000000000000000000000000000000;
+const B32_U64_C10_MSK: u64 = 0b0000000001111100000000000000000000000000000000000000000000000000;
+const B32_U64_C10_MIN: u64 = 0b0000000000000100000000000000000000000000000000000000000000000000;
+const B32_U64_C09_MSK: u64 = 0b0000000000000011111000000000000000000000000000000000000000000000;
+const B32_U64_C09_MIN: u64 = 0b0000000000000000001000000000000000000000000000000000000000000000;
+const B32_U64_C08_MSK: u64 = 0b0000000000000000000111110000000000000000000000000000000000000000;
+const B32_U64_C08_MIN: u64 = 0b0000000000000000000000010000000000000000000000000000000000000000;
+const B32_U64_C07_MSK: u64 = 0b0000000000000000000000001111100000000000000000000000000000000000;
+const B32_U64_C07_MIN: u64 = 0b0000000000000000000000000000100000000000000000000000000000000000;
+const B32_U64_C06_MSK: u64 = 0b0000000000000000000000000000011111000000000000000000000000000000;
+const B32_U64_C06_MIN: u64 = 0b0000000000000000000000000000000001000000000000000000000000000000;
+const B32_U64_C05_MSK: u64 = 0b0000000000000000000000000000000000111110000000000000000000000000;
+const B32_U64_C05_MIN: u64 = 0b0000000000000000000000000000000000000010000000000000000000000000;
+const B32_U64_C04_MSK: u64 = 0b0000000000000000000000000000000000000001111100000000000000000000;
+const B32_U64_C04_MIN: u64 = 0b0000000000000000000000000000000000000000000100000000000000000000;
+const B32_U64_C03_MSK: u64 = 0b0000000000000000000000000000000000000000000011111000000000000000;
+const B32_U64_C03_MIN: u64 = 0b0000000000000000000000000000000000000000000000001000000000000000;
+const B32_U64_C02_MSK: u64 = 0b0000000000000000000000000000000000000000000000000111110000000000;
+const B32_U64_C02_MIN: u64 = 0b0000000000000000000000000000000000000000000000000000010000000000;
+const B32_U64_C01_MSK: u64 = 0b0000000000000000000000000000000000000000000000000000001111100000;
+const B32_U64_C01_MIN: u64 = 0b0000000000000000000000000000000000000000000000000000000000100000;
+const B32_U64_C00_MSK: u64 = 0b0000000000000000000000000000000000000000000000000000000000011111;
+
+#[rustfmt::skip]
+pub fn encode_u64(n: u64) -> String {
+    let b = {
+        if n >= B32_U64_C12_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C12_MSK) >> 60) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C11_MSK) >> 55) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C10_MSK) >> 50) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C09_MSK) >> 45) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C08_MSK) >> 40) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C07_MSK) >> 35) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C11_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C11_MSK) >> 55) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C10_MSK) >> 50) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C09_MSK) >> 45) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C08_MSK) >> 40) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C07_MSK) >> 35) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C10_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C10_MSK) >> 50) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C09_MSK) >> 45) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C08_MSK) >> 40) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C07_MSK) >> 35) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C09_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C09_MSK) >> 45) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C08_MSK) >> 40) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C07_MSK) >> 35) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C08_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C08_MSK) >> 40) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C07_MSK) >> 35) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C07_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C07_MSK) >> 35) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C06_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C06_MSK) >> 30) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C05_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C05_MSK) >> 25) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C04_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C04_MSK) >> 20) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C03_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C03_MSK) >> 15) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C02_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C02_MSK) >> 10) as usize],
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else if n >= B32_U64_C01_MIN {
+            [
+                ENC_CROCKFORD_UPPER[((n & B32_U64_C01_MSK) >>  5) as usize],
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        } else {
+            [
+                ENC_CROCKFORD_UPPER[( n & B32_U64_C00_MSK       ) as usize],
+            ]
+            .to_vec()
+        }
+    };
+    unsafe { String::from_utf8_unchecked(b) }
+}
---
+++ src/lib.rs
@@ -0,0 +1,131 @@
+mod decode;
+mod encode;
+
+pub use crate::decode::decode_u64;
+pub use crate::encode::encode_u64;
+
+const INVALID_CHAR: char = '.';
+const INVALID_BYTE: u8 = u8::MAX;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::decode::DEC_CROCKFORD_UPPER;
+    use crate::encode::ENC_CROCKFORD_UPPER;
+
+    #[test]
+    fn both_u64_0() {
+        let n = 0;
+        let x = "0";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_31() {
+        let n = 31;
+        let x = "Z";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_32() {
+        let n = 32;
+        let x = "10";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_33() {
+        let n = 33;
+        let x = "11";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_32_32() {
+        let n = 32 * 32;
+        let x = "100";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_32_32_32_32() {
+        let n = 32 * 32 * 32 * 32;
+        let x = "10000";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_32_32_32_32_1() {
+        let n = (32 * 32 * 32 * 32) - 1;
+        let x = "ZZZZ";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_10239() {
+        let n = 10239;
+        let x = "9ZZ";
+        let s = encode_u64(n);
+        assert_eq!(s, x);
+        assert_eq!(decode_u64(s.as_bytes()).unwrap(), n);
+    }
+
+    #[test]
+    fn both_u64_low() {
+        for i in 0..ENC_CROCKFORD_UPPER.len() {
+            let s = encode_u64(i as u64);
+            // println!("{} vs {} vs {} vs {}", s, i, ENC_CROCKFORD_UPPER[i], ENC_CROCKFORD_UPPER[i] as char);
+            assert_eq!(s, (ENC_CROCKFORD_UPPER[i] as char).to_string());
+        }
+    }
+
+    #[test]
+    fn gen_crockford_upper_decode() {
+        let mut s = String::new();
+        for i in 0..=255u8 {
+            let c = i as char;
+            #[rustfmt::skip]
+            let c = match c {
+                '0' | 'o' | 'O' => '0',
+                '1' | 'i' | 'I' | 'l' | 'L' => '1',
+                '2' ..= '9' => c,
+                'u' | 'U' => INVALID_CHAR,
+                'A' ..= 'Z' => c,
+                'a' ..= 'z' => c.to_ascii_uppercase(),
+                _ => INVALID_CHAR,
+            };
+            s.push(c);
+        }
+
+        let dec_chars = DEC_CROCKFORD_UPPER
+            .map(|b| {
+                if b == INVALID_BYTE {
+                    INVALID_CHAR
+                } else {
+                    ENC_CROCKFORD_UPPER[b as usize] as char
+                }
+            })
+            .iter()
+            .collect::<String>();
+
+        assert_eq!(dec_chars, s);
+    }
+}
+
+// "The quick brown fox jumps over the lazy dog."
+// "AHM6A83HENMP6TS0C9S6YXVE41K6YY10D9TPTW3K41QQCSBJ41T6GS90DHGQMY90CHQPEBG"
